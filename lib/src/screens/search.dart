3cbfab2819884f61ca524e2009d424dc994e4859
Hypertext-Assassin-RSS <sanjayasenanayaka11@gmail.com>
2024-09-04 09:43:27 +0530

    add new search screen

---
+++ lib/src/screens/search.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:super_cupertino_navigation_bar/super_cupertino_navigation_bar.dart';
+
+class SearchScreen extends StatelessWidget {
+  const SearchScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoApp(
+      theme: CupertinoThemeData(),
+      home: Search(),
+    );
+  }
+}
+
+class Search extends StatefulWidget {
+  const Search({super.key});
+
+  @override
+  State<Search> createState() => _SearchState();
+}
+
+class _SearchState extends State<Search> {
+
+  Color _getBackgroundColor(BuildContext context) {
+    var brightness = MediaQuery.of(context).platformBrightness;
+    if (brightness == Brightness.dark) {
+      return CupertinoColors.black;
+    } else {
+      return CupertinoColors.white;
+    }
+  }
+
+  Color _getTextColor(BuildContext context) {
+    var brightness = MediaQuery.of(context).platformBrightness;
+    return brightness == Brightness.dark ? CupertinoColors.white : CupertinoColors.black;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var brightness = MediaQuery.of(context).platformBrightness;
+    bool isDarkMode = brightness == Brightness.dark;
+
+    return Scaffold(
+      backgroundColor: _getBackgroundColor(context),
+      body: SuperScaffold(
+        appBar: SuperAppBar(
+          title: Text(
+            "Explore",
+            style: TextStyle(
+              color: _getTextColor(context),
+            ),
+          ),
+          backgroundColor: _getBackgroundColor(context),
+          largeTitle: SuperLargeTitle(
+            enabled: true,
+            largeTitle: "Search",
+            textStyle: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: _getTextColor(context),
+            ),
+          ),
+          searchBar: SuperSearchBar(
+            enabled: true,
+            backgroundColor: isDarkMode ? CupertinoColors.darkBackgroundGray : CupertinoColors.lightBackgroundGray,
+            textStyle: TextStyle(
+              color: _getTextColor(context),
+            ),
+            onChanged: (query) {
+              // Search Bar Changes
+            },
+            onSubmitted: (query) {
+              // On Search Bar submitted
+            },
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
