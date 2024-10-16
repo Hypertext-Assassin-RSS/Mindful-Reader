3e413284462fd51e1a9d9381db70dca56d715a76
Hypertext-Assassin-RSS <sanjayasenanayaka11@gmail.com>
2024-10-16 10:38:20 +0530

    login fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:mindful_reader/src/screens/login.dart';
+import 'package:mindful_reader/src/widgets/bottomnav.dart';
 import './src/colors/color.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: "assets/config/.env");
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isLoggedIn = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkLoginStatus();
+  }
+
+  Future<void> _checkLoginStatus() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('token');
+
+    if (token != null && token.isNotEmpty) {
+      setState(() {
+        _isLoggedIn = true;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,10 +43,9 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Login',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        scaffoldBackgroundColor: KFifthColor
+        scaffoldBackgroundColor: KFifthColor,
       ),
-      // home: const BottomNavBar(),
-      home: const LoginScreen(),
+      home: _isLoggedIn ? const BottomNavBar() : const LoginScreen(),
     );
   }
 }
